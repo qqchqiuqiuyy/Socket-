4bd7fead7df14a78f2742e54be37e0be0e339b47
bobo_glb <ily520wan_glb>
2018-11-13 20:13:00 +0800

    test1

--- src/main/java/cn/bb/Apple.java
+++
@@ -1,8 +0,0 @@
-package cn.bb;
-
-public class Apple {
-	public static void main(String[] args) {
-		System.err.println("sadas");
-		System.err.println("hot fix");
-	}
-}
---
+++ src/main/java/cn/bb/Test01.java
@@ -0,0 +1,18 @@
+package cn.bb;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Test01 {
+	public static void main(String[] args) throws UnknownHostException {
+		//get InetAddress实例
+		InetAddress address = InetAddress.getLocalHost(); 
+		System.err.println("直接获取local: "+address);
+		
+		InetAddress inetAddress2 = InetAddress.getByName("Bobo");
+		System.out.println("通过主机名获取: "+inetAddress2.getHostName()+inetAddress2.getHostAddress());
+		
+		InetAddress addressByIp = InetAddress.getByName("10.30.14.60");
+		System.err.println("通过IP地址获取:"+addressByIp);
+	}
+}
